9709430bb21e2c6541594294209af27fda04d9a4
sdoublet <doubletsebastien@sfr.fr>
2018-09-17 18:16:35 +0200

    ajout du menu

---
+++ src/com/ocr/anthony/Order.java
@@ -0,0 +1,31 @@
+package com.ocr.anthony;
+
+public class Order {
+    /**
+     * Display all available menus in the restaurant.
+     */
+    public void displayAvailableMenu() {
+        System.out.println("Choix menu");
+        System.out.println("1 = poulet");
+        System.out.println("2 = boeuf");
+        System.out.println("3 = végétarien");
+        System.out.println("Que voulez voous comme menu?");
+    }
+    /**
+     * Display a selected menu.
+     * @param nbMenu The selected menu.
+     */
+    public void displaySelectedMenu(int nbMenu) {
+        if (nbMenu == 1) {
+            System.out.println("Vous avez choisi le menu : Poulet");
+        } else if (nbMenu == 2){
+            System.out.println("Vous avez choisi le menu : Boeuf");
+        } else if (nbMenu == 3){
+            System.out.println("Vous avez choisi le menu : Végétarien");
+        }else {
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
+
+
+    }
+}
